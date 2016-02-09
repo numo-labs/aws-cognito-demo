c5a1b2d644b664a36326659169abd02e77f72127
Eugeniy Boyarskiy <gekabrs@gmail.com>
2016-02-09 17:13:23 +0200

    implement cognito example

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './app/index'
+  ],
+  devtool: 'eval-source-map',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'app')
+      }
+    ]
+  }
+};
